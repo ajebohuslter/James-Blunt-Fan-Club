80dd8d7e6b1c60368d88702fc8e7b89a8b1a3c32
ajebohuslter <5amu4ld6vid@gmail.com>
2025-07-17 21:38:37 +0100

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,282 @@
+const fanComments = [
+  {
+    "email": "sara*********@gmail.com",
+    "comment": "James Blunt's music has been the soundtrack of my life for years. There's something timeless about his voice and lyrics."
+  },
+  {
+    "email": "emma*******@gmail.com",
+    "comment": "I remember crying to 'Goodbye My Lover' during a rough breakup. Thank you for the healing, James."
+  },
+  {
+    "email": "nina*****@icloud.com",
+    "comment": "Been a fan since 'Back to Bedlam' \u2013 and I\u2019ve never stopped listening. You\u2019re a legend."
+  },
+  {
+    "email": "alex******@gmail.com",
+    "comment": "That voice, those lyrics. James Blunt is a modern-day poet."
+  },
+  {
+    "email": "sara******@gmail.com",
+    "comment": "I don\u2019t know what magic is in your songs, James, but it always hits home."
+  },
+  {
+    "email": "nina*******@outlook.com",
+    "comment": "From 'You're Beautiful' to 'Monsters', your evolution is incredible. Proud to be a fan!"
+  },
+  {
+    "email": "zoe*********@gmail.com",
+    "comment": "Your live shows are on another level. Can't wait for the next tour!"
+  },
+  {
+    "email": "ken******@gmail.com",
+    "comment": "There\u2019s something comforting about hearing your songs on a rainy day."
+  },
+  {
+    "email": "dave********@outlook.com",
+    "comment": "James Blunt is underrated in so many ways. The man\u2019s a musical genius."
+  },
+  {
+    "email": "ella******@icloud.com",
+    "comment": "Your Instagram posts make my day. Funny and talented \u2013 unbeatable combo!"
+  },
+  {
+    "email": "john******@gmail.com",
+    "comment": "He makes heartbreak sound beautiful. That\u2019s talent."
+  },
+  {
+    "email": "alex******@gmail.com",
+    "comment": "Been to 3 concerts already and counting!"
+  },
+  {
+    "email": "mike*******@gmail.com",
+    "comment": "My mom and I bond over your songs. Generational love for James Blunt."
+  },
+  {
+    "email": "dave*********@gmail.com",
+    "comment": "Thank you for always being so real, James. Your authenticity shines through."
+  },
+  {
+    "email": "mike**********@gmail.com",
+    "comment": "\u2018The Afterlove\u2019 album is so underrated. I play it on loop."
+  },
+  {
+    "email": "nina**********@outlook.com",
+    "comment": "You're more than a singer \u2013 you're a storyteller."
+  },
+  {
+    "email": "dave*****@gmail.com",
+    "comment": "That concert in London 2023 was one of the best nights of my life."
+  },
+  {
+    "email": "zoe*******@gmail.com",
+    "comment": "You helped me through depression with just your music."
+  },
+  {
+    "email": "emma**********@gmail.com",
+    "comment": "Your voice is both soothing and haunting in the best way."
+  },
+  {
+    "email": "lisa*****@gmail.com",
+    "comment": "The emotion you pour into every performance is unmatched."
+  },
+  {
+    "email": "lisa**********@gmail.com",
+    "comment": "The lyrics in \u2018Same Mistake\u2019 still hit as hard as they did years ago."
+  },
+  {
+    "email": "ken*********@gmail.com",
+    "comment": "So grateful for your music, James. It's gotten me through so much."
+  },
+  {
+    "email": "john*********@gmail.com",
+    "comment": "If you ever need a fan backup singer, I'm ready!"
+  },
+  {
+    "email": "ella*********@gmail.com",
+    "comment": "I still have the ticket from your 2016 tour. Treasured memory."
+  },
+  {
+    "email": "mark********@gmail.com",
+    "comment": "Love from Brazil! Can\u2019t wait till you visit again."
+  },
+  {
+    "email": "zoe*******@gmail.com",
+    "comment": "\u2018Monsters\u2019 made me call my dad and cry. Thank you."
+  },
+  {
+    "email": "sara**********@gmail.com",
+    "comment": "I propose James Blunt as an international treasure."
+  },
+  {
+    "email": "nina******@gmail.com",
+    "comment": "Each song feels like a personal letter. That\u2019s rare."
+  },
+  {
+    "email": "mike*****@gmail.com",
+    "comment": "You're not just an artist; you're a friend to every listener."
+  },
+  {
+    "email": "dave**********@gmail.com",
+    "comment": "Even after all these years, the magic hasn\u2019t faded."
+  },
+  {
+    "email": "lisa********@gmail.com",
+    "comment": "Thanks for being the voice of our heartache and our healing."
+  },
+  {
+    "email": "lisa*****@gmail.com",
+    "comment": "Your humor is as powerful as your music. Big respect!"
+  },
+  {
+    "email": "lisa********@gmail.com",
+    "comment": "Every time I hear your voice, I feel calm."
+  },
+  {
+    "email": "dave*****@gmail.com",
+    "comment": "\u2018High\u2019 is still one of my favorite songs ever."
+  },
+  {
+    "email": "mike**********@gmail.com",
+    "comment": "From pop to acoustic \u2013 you\u2019ve mastered it all."
+  },
+  {
+    "email": "dave*********@gmail.com",
+    "comment": "Thank you for singing the things we\u2019re too scared to say."
+  },
+  {
+    "email": "emma*****@gmail.com",
+    "comment": "Always classy, always heartfelt \u2013 that's James."
+  },
+  {
+    "email": "emma**********@gmail.com",
+    "comment": "Seeing you live changed how I experience music."
+  },
+  {
+    "email": "nina********@gmail.com",
+    "comment": "\u2018Carry You Home\u2019 brings tears every time. Pure brilliance."
+  },
+  {
+    "email": "john*********@gmail.com",
+    "comment": "Your fan club feels like a warm hug."
+  },
+  {
+    "email": "sara*******@gmail.com",
+    "comment": "Never stop making music. We need your songs."
+  },
+  {
+    "email": "dave********@gmail.com",
+    "comment": "Been a fan for 15 years \u2013 and counting."
+  },
+  {
+    "email": "lisa*******@gmail.com",
+    "comment": "Can we get more acoustic versions, please?"
+  },
+  {
+    "email": "zoe*********@gmail.com",
+    "comment": "The raw emotion in \u2018Goodbye My Lover\u2019 \u2013 legendary."
+  },
+  {
+    "email": "emma********@gmail.com",
+    "comment": "You deserve more Grammys, James."
+  },
+  {
+    "email": "zoe*********@outlook.com",
+    "comment": "We fans love you more than words can say."
+  },
+  {
+    "email": "mike******@gmail.com",
+    "comment": "Listening to you is like therapy."
+  },
+  {
+    "email": "mike**********@gmail.com",
+    "comment": "James Blunt = timeless music."
+  },
+  {
+    "email": "nina*******@gmail.com",
+    "comment": "There\u2019s no one like you out there."
+  },
+  {
+    "email": "alex*******@icloud.com",
+    "comment": "Love from the Philippines!"
+  },
+  {
+    "email": "nina**********@gmail.com",
+    "comment": "One of the few artists who always delivers."
+  },
+  {
+    "email": "ella**********@outlook.com",
+    "comment": "Keep being unapologetically yourself."
+  },
+  {
+    "email": "mike*******@gmail.com",
+    "comment": "Even my dog loves your music."
+  },
+  {
+    "email": "ella********@gmail.com",
+    "comment": "How can one artist have so many hits?"
+  },
+  {
+    "email": "zoe********@yahoo.com",
+    "comment": "Never boring, always heartfelt."
+  },
+  {
+    "email": "dave********@icloud.com",
+    "comment": "Came for \u2018You\u2019re Beautiful\u2019, stayed for everything else."
+  },
+  {
+    "email": "zoe*********@gmail.com",
+    "comment": "James, you\u2019re simply the best."
+  },
+  {
+    "email": "mike********@icould.com",
+    "comment": "Thanks for staying humble and funny."
+  },
+  {
+    "email": "lisa******@gmail.com",
+    "comment": "Wish I could attend every concert."
+  },
+  {
+    "email": "zoe******@gmail.com",
+    "comment": "My wedding song was \u2018Bonfire Heart\u2019. Forever grateful."
+  },
+  {
+    "email": "lisa********@outlook.com",
+    "comment": "Legends don\u2019t need introductions \u2013 James Blunt."
+  },
+  {
+    "email": "alex*******@icloud.com",
+    "comment": "Still hoping to meet you one day."
+  },
+  {
+    "email": "emma**********@gmail.com",
+    "comment": "Thank you for being so active with fans."
+  },
+  {
+    "email": "mike********@gmail.com",
+    "comment": "\u2018The Greatest\u2019 is the anthem we didn\u2019t know we needed."
+  },
+  {
+    "email": "zoe*****@gmail.com",
+    "comment": "James, your lyrics saved lives \u2013 mine included."
+  },
+  {
+    "email": "ella*******@gmail.com",
+    "comment": "You're more than a celebrity. You're family to us."
+  },
+  {
+    "email": "emma*******@gmail.com",
+    "comment": "Much love from New York!"
+  },
+  {
+    "email": "dave**********@icloud.com",
+    "comment": "Music with meaning \u2013 that\u2019s why I\u2019m here."
+  },
+  {
+    "email": "alex******@gmail.com",
+    "comment": "James, you give hope through melodies."
+  },
+  {
+    "email": "ella**********@gmail.com",
+    "comment": "You've never released a bad song. Ever."
+  }
+];
